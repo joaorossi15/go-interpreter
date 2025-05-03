397dbe0b51598f667bb7bf103c4074702b2ff5ba
joaorossi15 <joaorossiborba@gmail.com>
2025-05-03 15:56:14 -0300

    .

--- src/monkey/repl/repl.go
+++ src/monkey/repl/repl.go
@@ -21,6 +21,35 @@ const MONKEY_FACE = `
          m  m
 `
 
+const PROMPT = `>> `
+
+func CheckParser(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.NewParser(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
+
 func REPL(in io.Reader, out io.Writer) error {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnviroment()
